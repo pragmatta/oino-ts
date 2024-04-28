6053bfbe87da22612b9d8d8365e17eb161981477
Matias Kiviniemi <matias.kiviniemi@gmail.com>
2024-04-28 19:17:42 +0300

    Added sample app for the readme-content.

---
+++ samples/readmeApp/readme.ts
@@ -0,0 +1,46 @@
+import { OINODb, OINOApi, OINOFactory, OINOLog, OINOLogLevel, OINOConsoleLog, OINOApiResult, OINOContentType, OINOSwagger, OINORequestParams } from "@oino-ts/core";
+
+import { OINODbBunSqlite } from "@oino-ts/bunsqlite"
+
+OINOFactory.registerDb("OINODbBunSqlite", OINODbBunSqlite)
+OINOLog.registerLogger("OINOConsoleLog", OINOConsoleLog)
+OINOLog.setLogLevel(OINOLogLevel.debug)
+
+const db:OINODb = await OINOFactory.createDb( { type: "OINODbBunSqlite", url: "file://./northwind.sqlite" } )
+const apis:Record<string, OINOApi> = {
+    "employees": await OINOFactory.createApi(db, { tableName: "Employees", excludeFields:["BirthDate"] }),
+    "orders": await OINOFactory.createApi(db, { tableName: "Orders" }),
+    "orderdetails": await OINOFactory.createApi(db, { tableName: "OrderDetails" }),
+    "products": await OINOFactory.createApi(db, { tableName: "Products" })
+}
+const api_array:OINOApi[] = Object.entries(apis).map(([path, api]) => (api))
+
+Bun.serve({
+    port: 3001,
+    async fetch(request) {
+        let url = new URL(request.url.toLowerCase())
+        let path_matches = url.pathname.match(/\/(\w+)\/?(.+)?/i) || []
+        OINOLog.debug("readmeApp serve", {path:url.pathname, path_matches:path_matches})
+        let path:string = path_matches[1] || ""
+        let id:string = path_matches[2] || ""
+        let api:OINOApi = apis[path]
+
+        if (url.pathname == "/swagger.json") {
+            return new Response(JSON.stringify(OINOSwagger.getApiDefinition(api_array)))
+            
+        } else if (!api) {
+            return new Response("No api for URL " + url.pathname)
+
+        } else {
+            const body:string = await request.text()
+            const params:OINORequestParams = OINOFactory.createRequestParamsFromUrl(new URL(request.url))
+            const result:OINOApiResult = await api.doRequest(request.method, id, body, params)
+            if (result.success && result.modelset) {
+                return new Response(result.modelset.writeString(params.contentType || OINOContentType.json))
+            } else {
+                return new Response(JSON.stringify(result))
+            }
+
+        }
+    },
+})
