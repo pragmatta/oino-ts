eda7344e32216be0105090646bf6c38e391017dc
Matias Kiviniemi <matias.kiviniemi@gmail.com>
2024-10-23 20:34:37 +0300

    Update htmxApp to host also files in port 8080

--- samples/htmxApp/htmxApp.ts
+++ samples/htmxApp/htmxApp.ts
@@ -1,5 +1,7 @@
-import { OINODb, OINODbParams, OINODbApi, OINODbFactory, OINOConsoleLog, OINOBenchmark, OINODbSwagger, OINODbApiResult, OINORequestParams, OINOLog, OINOLogLevel } from "@oino-ts/db";
+import { OINODb, OINODbParams, OINODbApi, OINODbFactory, OINOConsoleLog, OINOBenchmark, OINODbSwagger, OINODbApiResult, OINOLog, OINOLogLevel, OINODbHtmlTemplate, OINODbApiRequestParams } from "@oino-ts/db";
 
+import { OINODbConfig } from "@oino-ts/db"
+import { OINOHttpResult, OINOHtmlTemplate } from "@oino-ts/types"
 import { OINODbBunSqlite } from "@oino-ts/db-bunsqlite"
 import { BunFile } from "bun";
 import { existsSync, readFileSync } from "fs";
@@ -15,7 +17,7 @@ OINOLog.setLogger(new OINOConsoleLog(OINOLogLevel.debug))
 OINODbFactory.registerDb("OINODbBunSqlite", OINODbBunSqlite)
 
 const API_PATH_REGEX = /\/([^\/]*)\/?([^\/]*)\/?([^\/]*)/
-async function findBestTemplateId(apiName:string, operation:string, statusCode:string):BunFile|null {
+async function findBestTemplateId(apiName:string, operation:string, statusCode:string):Promise<BunFile|null> {
 	let template_id:string = apiName.toLowerCase()
 	if (operation) {
 		template_id += "-" + operation.toLowerCase()
@@ -30,7 +32,7 @@ async function findBestTemplateId(apiName:string, operation:string, statusCode:s
 	}
 	return null
 }
-async function getTemplate(apiName:string, method:string, command:string, statusCode:string):Promise<string> {
+async function getTemplate(apiName:string, method:string, command:string, statusCode:string):Promise<OINODbHtmlTemplate> {
 	OINOLog.debug("index.ts / getTemplate", { apiName:apiName, method:method, command:command, statusCode:statusCode})
 	const template_file = await findBestTemplateId(apiName, command, statusCode) ||
 						await findBestTemplateId(apiName, method, statusCode) ||
@@ -38,19 +40,18 @@ async function getTemplate(apiName:string, method:string, command:string, status
 						await findBestTemplateId(apiName, method, "") ||
 						await findBestTemplateId(apiName, "", "") || ""
 	if (template_file) {
-		return await template_file.text()
-		// TEMPLATE_CACHE[id] = result
+		const html:string = await template_file.text()
+		return new OINODbHtmlTemplate(html)
 	}	
-	return ""
+	return new OINODbHtmlTemplate("")
 }
 
-function hostFile(path: string, contentType: string, data?: any): Response {
+function hostFile(path: string, contentType: string, data?:any): Response {
 	if (existsSync("." + path)) {
-		let body: string = readFileSync("." + path, { encoding: "utf8" });
-		if (data) {
-			body = OINODbFactory.createHtmlFromObject(data, body);
-		}
-		return new Response(body, { status: 200, statusText: "OK", headers: { "Content-Type": contentType, "Content-Length": body.length.toString() } });
+		let file_content: string = readFileSync("." + path, { encoding: "utf8" });
+		const template:OINOHtmlTemplate = new OINOHtmlTemplate(file_content)
+		const http_result = template.renderFromObject(data)
+		return http_result.getResponse( { "Content-Type": contentType })
 	} else {
 		return new Response("", { status: 404, statusText: "File not found" });
 	}
@@ -58,7 +59,7 @@ function hostFile(path: string, contentType: string, data?: any): Response {
 
 try {
 
-	const db_params:OINODbParams = { type: "OINODbBunSqlite", url: "file://./northwind.sqlite" }
+	const db_params:OINODbParams = { database: "northwind", type: "OINODbBunSqlite", url: "file://./northwind.sqlite" }
 	const db:OINODb = await OINODbFactory.createDb(db_params)
 
 	const apis:Record<string, OINODbApi> = {
@@ -72,7 +73,7 @@ try {
 		
 	const server = Bun.serve({
 		development: true,
-		port: 3002,
+		port: 8080,
 		async fetch(request:Request) {
 			let url = new URL(request.url)
 			OINOLog.debug("index.ts / fetch", {url:url, pathname:url.pathname, headers:request.headers, method:request.method }) 
@@ -80,6 +81,9 @@ try {
 			let response:Response|null = null
 			if (request.method == "OPTIONS") {
 				return new Response("", {status:200, statusText:"OK", headers:response_headers})
+
+			} else if (url.pathname == "/") {
+				return new Response("", {status:302, statusText:"OK", headers:{"Location": "/index.html"}})
 
 			} else if (url.pathname == "/swagger.json") {
 				response = new Response(JSON.stringify(OINODbSwagger.getApiDefinition(api_array), null, 5));
@@ -97,30 +101,30 @@ try {
 				const operation:string = path_matches[3]?.toLowerCase() || ""
 				OINOLog.debug("index.ts / request", {api_name:api_name, id:id, operation:operation }) 
 
-				const params:OINORequestParams = OINODbFactory.createParamsFromRequest(request)
+				const params:OINODbApiRequestParams = OINODbFactory.createParamsFromRequest(request)
 				const api:OINODbApi|null = apis[api_name]
 				const body = await request.text()
 				OINOLog.debug("index.ts / api", {params:params, body:body }) 
 				let api_result:OINODbApiResult
 				if (api_name == "") {
-					const template:string = await getTemplate(id, "", operation, "")
+					const template:OINODbHtmlTemplate = await getTemplate(id, "", operation, "")
 					if (template) {
-						const html:string = OINODbFactory.createHtmlFromOinoId(id, template)
-						response = new Response(html, {status:200, statusText: "OK", headers: response_headers })	
+						const http_result:OINOHttpResult = template.renderFromKeyValue(OINODbConfig.OINODB_ID_FIELD, id)
+						response = http_result.getResponse(response_headers)
 					} else {
 						response = new Response("Template not found!", {status:404, statusText: "Template not found!", headers: response_headers })	
 					}
 				} else if (api) {
 					api_result = await api.doRequest(request.method, id, body, params)
-					const template:string = await getTemplate(api.params.tableName, request.method, operation, api_result.statusCode.toString())
+					const template:OINODbHtmlTemplate = await getTemplate(api.params.tableName, request.method, operation, api_result.statusCode.toString())
 					OINOLog.debug("index.ts / template", {template:template}) 
-					let html:string
+					let http_result:OINOHttpResult
 					if (api_result.data?.dataset) {
-						html = OINODbFactory.createHtmlFromData(api_result.data, template)
+						http_result = template.renderdFromDbData(api_result.data)
 					} else {
-						html = OINODbFactory.createHtmlFromOinoId(id, template)
+						http_result = template.renderFromKeyValue(OINODbConfig.OINODB_ID_FIELD, id)
 					}
-					response = new Response(html, {status:api_result.statusCode, statusText: api_result.statusMessage, headers: response_headers })
+					response = http_result.getResponse(response_headers)
 					if (request.method == "POST") {
 						response.headers.set('HX-Trigger', 'OINODbApiTrigger-' + api.params.tableName)
 					} else if ((request.method == "PUT") || (request.method == "DELETE")) {
