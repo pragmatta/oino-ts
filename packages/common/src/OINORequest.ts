02440191c7f460103fd860a49da839850ce1a92e
Matias Kiviniemi <matias.kiviniemi@gmail.com>
2026-01-31 15:31:12 +0200

    Refactored data as body to be more interface compliant

--- packages/common/src/OINORequest.ts
+++ packages/common/src/OINORequest.ts
@@ -36,7 +36,7 @@ export interface OINOHttpRequestInit extends OINORequestInit {
     url?: URL
     method?: string
     headers?: OINOHeadersInit
-    data?: string|Buffer|Uint8Array|object|null
+    body?: string|Buffer|Uint8Array|object|null|undefined
     requestType?:OINOContentType
     responseType?:OINOContentType
     multipartBoundary?:string
@@ -50,7 +50,7 @@ export class OINOHttpRequest extends OINORequest {
     readonly url?: URL
     readonly method: string
     readonly headers: OINOHeaders
-    readonly data: string|Buffer|Uint8Array|object|null
+    readonly body: string|Buffer|Uint8Array|object|null|undefined
     readonly requestType:OINOContentType
     readonly responseType:OINOContentType
     readonly multipartBoundary?:string
@@ -68,7 +68,7 @@ export class OINOHttpRequest extends OINORequest {
         this.url = init.url 
         this.method = init.method ?? "GET"
         this.headers = new OINOHeaders(init.headers)
-        this.data = init.data ?? ""
+        this.body = init.body 
         this.multipartBoundary = ""
         this.lastModified = init.lastModified
 
@@ -118,51 +118,73 @@ export class OINOHttpRequest extends OINORequest {
         }
     }
 
+    /**
+     * Creates a `OINOHttpRequest` from a Fetch API `Request` object.
+     * 
+     * @param request Fetch request
+     *  
+     */
     static async fromFetchRequest(request: Request): Promise<OINOHttpRequest> {
         const body = await request.arrayBuffer()
         return new OINOHttpRequest({
             url: new URL(request.url),
             method: request.method,
             headers: Object.fromEntries(request.headers as any),
-            data: Buffer.from(body),
+            body: Buffer.from(body),
         })
     }
 
-    dataAsText(): string {
-        if (this.data instanceof Uint8Array) {
-            return new TextDecoder().decode(this.data)
+    /**
+     * Returns the request data as a text string.
+     * 
+     */
+    bodyAsText(): string {
+        if (this.body instanceof Uint8Array) {
+            return new TextDecoder().decode(this.body)
 
-        } else if (this.data instanceof Object) {
-            return JSON.stringify(this.data)
+        } else if (this.body instanceof Object) {
+            return JSON.stringify(this.body)
             
         } else {
-            return this.data?.toString() || ""
+            return this.body?.toString() || ""
         }
     }
 
-    dataAsParsedJson(): any {
-        return this.data ? JSON.parse(this.dataAsText()) : {}
+    /**
+     * Returns the request data parsed as JSON object.
+     * 
+     */
+    bodyAsParsedJson(): any {
+        return this.body ? JSON.parse(this.bodyAsText()) : {}
     }
 
-    dataAsFormData(): URLSearchParams {
-        return new URLSearchParams(this.dataAsText() || "")
+    /**
+     * Returns the request data as URLSearchParams (form data).
+     * 
+     */
+    bodyAsFormData(): URLSearchParams {
+        return new URLSearchParams(this.bodyAsText() || "")
     }
-
-    dataAsBuffer(): Buffer {
-        if (this.data === null) {
+    
+    /**
+     * Returns the request data as Buffer.
+     * 
+     */
+    bodyAsBuffer(): Buffer {
+        if ((this.body === null) || (this.body === undefined)) {
             return Buffer.alloc(0)
 
-        } else if (this.data instanceof Buffer) {
-            return this.data
+        } else if (this.body instanceof Buffer) {
+            return this.body
 
-        } else if (this.data instanceof Uint8Array) {
-            return Buffer.from(this.data)
+        } else if (this.body instanceof Uint8Array) {
+            return Buffer.from(this.body)
 
-        } else if (this.data instanceof Object) {
-            return Buffer.from(JSON.stringify(this.data), "utf-8")
+        } else if (this.body instanceof Object) {
+            return Buffer.from(JSON.stringify(this.body), "utf-8")
 
         } else {
-            return Buffer.from(this.data, "utf-8")
+            return Buffer.from(this.body, "utf-8")
         }
     }
 
